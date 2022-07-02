ced1fc1fcb58dceb121264c6161dc9d2ef5602f1
2000-jessi <jessica2_bece@yahoo.com>
2022-07-02 11:44:23 -0500

    Cambios en audifonos.service

--- routes/products.js
+++ routes/products.js
@@ -15,6 +15,37 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res)=>{
+  try {
+    let body = req.body;
+    await audifonos.registerAudifonos(body)
+    res.status(201).json({
+      message: "Create Successfully",
+  })
+  } catch (error) {
+    throw error
+  }
+})
+
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params
+  const body = req.body
+  console.log(id)
+  console.log(body)
+  await audifonos.updateAudifonos(id, body)
+  res.status(201).json({
+      message: "Update successfully"
+  })
+})
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params
+  await audifonos.deleteAudifonos(id)
+  res.status(200).json({
+      message: "Delete Successfull"
+  })
+})
+
 
 
 module.exports = router;
--- services/audifonos.service.js
+++ services/audifonos.service.js
@@ -2,8 +2,16 @@
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const { initializeApp } = require("firebase/app")
-const { getFirestore, collection, getDocs, updateDoc, doc, deleteDoc, addDoc } = require('firebase/firestore');
+const { initializeApp } = require("firebase/app");
+const {
+  getFirestore,
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  deleteDoc,
+  addDoc,
+} = require("firebase/firestore");
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,50 +25,79 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+const db = getFirestore(app);
 
-class AudifonosService{
-    async getDataUsers() {
-        try {
-            const usuarios = collection(db, "usuarios")
-            const response = await getDocs(usuarios);
-            const ids = response.docs.map(doc => doc.id)
-            const data = response.docs.map(doc => doc.data());
-            let result = []
-            for (let i = 0; i < data.length; i++) {
-                result.push({ id: ids[i], ...data[i] })
-            }
-            return result;
-        } catch (error) {
-            throw error
-        }
+class AudifonosService {
+  async getDataUsers() {
+    try {
+      const usuarios = collection(db, "usuarios");
+      const response = await getDocs(usuarios);
+      const ids = response.docs.map((doc) => doc.id);
+      const data = response.docs.map((doc) => doc.data());
+      let result = [];
+      for (let i = 0; i < data.length; i++) {
+        result.push({ id: ids[i], ...data[i] });
+      }
+      return result;
+    } catch (error) {
+      throw error;
     }
-    async getDataAudifonos(id){
-        try{
-            const audifonos = collection(db,"audifonos")
-            const response = await getDocs(audifonos)
-            const data = response.docs.map(doc => doc.data())
-            let result=[]
-            for (let i = 0; i < data.length; i++) {
-                if(data[i].usuario===id){
-                    result.push(data[i])
-                }
-            }
-            return result;
-        }catch(error){
-            throw error
+  }
+  async getDataAudifonos(id) {
+    try {
+      const audifonos = collection(db, "audifonos");
+      const response = await getDocs(audifonos);
+      const ids = response.docs.map((doc) => doc.id);
+      const data = response.docs.map((doc) => doc.data());
+      let result = [];
+      for (let i = 0; i < data.length; i++) {
+        result.push({ id: ids[i], ...data[i] });
+        if (data[i].usuario != id) {
+          result.splice(i, 1);
         }
+      }
+      return result;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async registerUser(body){
-        try {
-            const usuarios = collection(db, "usuarios")
-            await addDoc(usuarios,body)
-        } catch (error) {
-            throw error
-        }
+  async registerUser(body) {
+    try {
+      const usuarios = collection(db, "usuarios");
+      await addDoc(usuarios, body);
+    } catch (error) {
+      throw error;
     }
+  }
 
+  async registerAudifonos(body) {
+    try {
+      const audifonos = collection(db, "audifonos");
+      await addDoc(audifonos, body);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateAudifonos(id, body) {
+    try {
+      console.log(body);
+      const audifonos = doc(db, "audifonos", id);
+      await updateDoc(audifonos, body);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteAudifonos(id) {
+    try {
+      const audifonos = doc(db, "audifonos", id);
+      await deleteDoc(audifonos);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
-module.exports = AudifonosService
+module.exports = AudifonosService;
